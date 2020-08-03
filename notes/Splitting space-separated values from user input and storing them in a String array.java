881a4fcad4eeec1271b18e6e4815119f91b4ca3e
Anubhav Talukdar <35625758+AnubhavTalukdar@users.noreply.github.com>
2020-08-04 03:41:00 +0530

    Update Splitting space-separated values from user input and storing them in a String array.java

--- notes/Splitting space-separated values from user input and storing them in a String array.java
+++ notes/Splitting space-separated values from user input and storing them in a String array.java
@@ -1,15 +1,25 @@
-import java.util.*;
+import java.io.*;
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args)throws IOException
 	{
-	    Scanner sc = new Scanner(System.in);
-	    int i;
-	    String str[] = sc.nextLine().split(" ");
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
-	    for(i=0; i<str.length;i++)
+	    String str[] = br.readLine().split(" ");
+	    int i, arr[] = new int[str.length];
+	    
+	    
+	   // Converting all String inputs to numbers
+	   
+   	    for(i=0; i<str.length;i++)
 	    {
-	        System.out.println(str[i]);
+	        arr[i] = Integer.parseInt(str[i]);
+	    }
+	        
+	    
+	    for(i=0; i<arr.length;i++)
+	    {
+	        System.out.println(arr[i]);
 	    }
 
 	}
